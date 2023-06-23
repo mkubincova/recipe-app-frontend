42e25d6145cc720181ee0e58acba7da4ab8f4854
mkubincova <mkubincova@proton.me>
2023-06-23 11:10:38 +0200

    feat: initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import SiteHeader from "./components/SiteHeader";
+import Homepage from "./pages/Homepage";
+import Recipe from "./pages/Recipe";
+import Category from "./pages/Category";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <SiteHeader />
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route path="/recipe/:id" element={<Recipe />} />
+          <Route path="/category/:id" element={<Category />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
