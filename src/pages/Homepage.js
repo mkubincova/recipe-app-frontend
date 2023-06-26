866a99d78a1d0f2ee1b81a29f13e5dd7e7546f8e
mkubincova <mkubincova@proton.me>
2023-06-26 12:51:47 +0200

    feat: fetch all recipes

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import useFetch from '../hooks/useFetch';
+import { Link } from 'react-router-dom';
 
 export default function Homepage() {
+    const { loading, error, data } = useFetch('http://localhost:1337/api/recipes?populate=cover');
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error</p>;
+
     return (
-        <div>Homepage</div>
+        <div>
+            {data.data.map(recipe => (
+                <div key={recipe.id} className='recipe-card'>
+                    <div className="category-icon">i</div>
+                    <div>
+                        <h2>{recipe.attributes.title}</h2>
+                        <small>console list</small>
+                        <Link to={`/recipe/${recipe.id}`}>View recipe</Link>
+                    </div>
+                    {recipe.attributes.cover.data ?
+                        <div className='image-container'>
+                            {/* {console.log(recipe.attributes.cover.data.attributes.formats)} */}
+                            <img src={`http://localhost:1337${recipe.attributes.cover.data?.attributes?.formats?.thumbnail?.url}`}
+                                alt={recipe.attributes.cover.data?.attributes?.alternativeText}
+                                width="100"
+                                height="100"
+                            />
+                        </div> : <></>
+                    }
+                </div>
+            ))}
+        </div>
     );
 }
